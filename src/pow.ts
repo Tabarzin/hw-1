a6b33260811d3e2088da9182004ff19579d8f5a4
Rustam <tabarzin@gmail.com>
2023-02-09 00:31:20 +0300

    feat: solve pow and sum

--- src/pow.ts
+++ src/pow.ts
@@ -1,3 +1,25 @@
-const pow = () => {};
+const curry =(fn) =>{
+    let curried;
+    return curried = (...args) => {
+        if (fn.length !== args.length){
+            return curried.bind(null, ...args)
+        }
+    return fn(...args);
+    };
+}
+
+
+
+
+const doPow = (base, exponent) => {
+  if(typeof(base) !== 'number' || typeof(exponent) !== 'number') {
+    throw new Error('INVALID_ARGUMENT')
+  }
+   return Math.pow(base, exponent)
+  }
+
+const pow = curry(doPow);
+
+
 
 export default pow;
--- src/sum.ts
+++ src/sum.ts
@@ -1,3 +1,18 @@
-const sum = () => {};
+const sum = (...args) => {
+
+  if(args.length < 2 ) throw new Error('INVALID_ARGUMENTS_COUNT')
+ 
+  let result = 0;
+
+  for (let arg of args) {
+    if(typeof(arg) !== 'number') {
+      throw new Error('INVALID_ARGUMENT')
+    } else { 
+      result = result + arg;
+    }
+  } 
+
+  return result;
+};
 
 export default sum;
