cb8ee17308a57e01d2d71434721691b50ca45757
Rustam <tabarzin@gmail.com>
2023-02-10 00:06:11 +0300

    feat: solve multiply

--- src/multiply.ts
+++ src/multiply.ts
@@ -1,3 +1,11 @@
-const multiply = () => {};
+function multiply(a) {
+    if(typeof a !== 'number' ) throw new Error('INVALID_ARGUMENT')
+    return function(b) {
+        if(typeof b !== 'number') throw new Error('INVALID_ARGUMENT')
+        return a * b;
+    }
+}
+
+
 
 export default multiply;
