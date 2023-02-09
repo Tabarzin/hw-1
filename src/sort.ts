36f1a0e67a8a995366d8944f94024fd0c239295f
Rustam <tabarzin@gmail.com>
2023-02-09 16:05:28 +0300

    feat: solve sort

--- src/sort.ts
+++ src/sort.ts
@@ -1,3 +1,14 @@
-const sort = () => {};
+const sort = (s) => {
+  if(typeof(s) !== 'string') throw new Error('INVALID_ARGUMENT');
+ 
+  let str = s.toLowerCase();
+  let arr = str.split(' ');
+  
+  arr.sort((a, b) => a.length < b.length ? -1 : a > b ? 1 : 0);
+
+  let newArr = arr.map((i) => i.split('').sort((a, b) => a < b ? -1 : a > b ? 1 : 0).join(''))
+
+  return newArr.join(' ');
+};
 
 export default sort;
