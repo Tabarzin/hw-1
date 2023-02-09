5b67f3c1c68374d30aa53da1cacd19acfab6a936
Rustam <tabarzin@gmail.com>
2023-02-09 22:23:35 +0300

    feat: solve anagrams

--- src/removeAnagrams.ts
+++ src/removeAnagrams.ts
@@ -1,3 +1,20 @@
-const removeAnagrams = () => {};
+const removeAnagrams = (arr) => {
+    if(!(Array.isArray(arr))) throw new Error('INVALID_ARGUMENT');
+
+
+ const obj = {};
+  
+    for (const word of arr) {
+        if(typeof word !== 'string') throw new Error('INVALID_ELEMENT_IN_ARRAY');
+        let sorted = word.toLowerCase().split("").sort().join("");
+        obj[sorted] = sorted in obj ? undefined : word;
+    }
+
+    return Object.values(obj).filter(Boolean);
+
+};
 
 export default removeAnagrams;
+
+
+
