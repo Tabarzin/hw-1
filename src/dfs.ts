0e63460262965ab60f4cf274aad4e1398877b19f
Rustam <tabarzin@gmail.com>
2023-02-10 20:48:05 +0300

    feat: solve dfs

--- src/dfs.ts
+++ src/dfs.ts
@@ -1,3 +1,20 @@
-const dfs = () => {};
+const dfs = (tree) => {
+
+if (typeof tree !== 'object' || tree === null) throw new Error('INVALID_ARGUMENT');
+
+const res: string[] = [];
+
+const fillRes = (i) => {
+    res.push(i);
+
+    tree[i].forEach(fillRes);
+
+};
+
+fillRes(Object.keys(tree)[0]);
+return res;
+
+};
 
 export default dfs;
+
