fb0df8085c844b882d6af1cf3e9999df357e738b
Rustam <tabarzin@gmail.com>
2023-02-10 20:56:17 +0300

    feat: solve bfs

--- src/bfs.ts
+++ src/bfs.ts
@@ -1,3 +1,21 @@
-const bfs = () => {};
+const bfs = (tree) => {
+if (typeof tree !== 'object' || tree === null) throw new Error('INVALID_ARGUMENT');
+
+let res: string[] = [];
+let arr = [Object.keys(tree)[0]];
+
+  while (arr.length > 0) {
+    let i = arr.shift() as string;
+
+    res.push(i);
+
+    if (tree[i].length !== 0) {
+      tree[i].forEach((br) => arr.push(br));
+    }
+  }
+
+  return res;
+
+};
 
 export default bfs;
