44de0e71d0125138eb89de1e2d0dfcf6add7b805
Rustam <tabarzin@gmail.com>
2023-02-10 19:39:32 +0300

    feat: solve patchArr

--- src/patchArrays.ts
+++ src/patchArrays.ts
@@ -1,5 +1,37 @@
 // @ts-nocheck
 
-const patchArrays = (): void => {};
+const patchArrays = (): void => {
+
+
+  Array.prototype.count = function() {
+    let res = 0;
+    for(let i = 0; i < this.length; i++){
+      res++
+    }
+    return res
+}
+  
+ Array.prototype.insert = function(index) {
+    if(typeof index !== 'number') throw new Error('INVALID_ARGUMENT');
+    if(index < 0) index = 0;
+    this.splice.apply(this, [index, 0].concat(
+        Array.prototype.slice.call(arguments, 1)));
+    return this;
+};
+    
+Array.prototype.remove = function(a) {
+  for (let i = 0; i < this.length; i++) {
+    if(a === this[i]) {this.splice(i, 1);
+    return this;
+    }
+    if(!(this.includes(a))) return this
+  } 
+  return this
+  
+}
+
+
+
+};
 
 export default patchArrays;
